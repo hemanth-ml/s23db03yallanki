2d6d71fda65439ae64beb82ef074829e78353d80
hemanth chowdary <yallankihemanth719@gmail.com>
2023-11-08 23:09:53 -0600

    added app file

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var moviesRouter = require('./routes/movies');
+var movieRouter = require('./routes/movie');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+var resourceRouter = require('./routes/resource');
+var movie = require('./models/movie');
+
+
 
 var app = express();
 
@@ -24,9 +36,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/movies', moviesRouter);
+app.use('/movie', movieRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use('/resource', resourceRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +57,37 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// We can seed the collection if needed onserver start
+async function recreateDB(){
+  // Delete everything
+  //  await Devices.deleteMany();
+  let instance1 = new
+  movie({movieName:"Inter stellar",movieRelasedDate:2013,moviePrice:20.0});
+  instance1.save().then(doc=>{
+  console.log("First object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  
+  let instance2 = new
+  movie({movieName:"Tales",movieRelasedDate:2014,moviePrice:10.0});
+  instance2.save().then(doc=>{
+  console.log("secound object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  let instance3 = new
+  movie({movieName:"RRR",movieRelasedDate:2015,moviePrice:22.0});
+  instance3.save().then(doc=>{
+  console.log("Third object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+  }
+  let reseed = true;
+  if (reseed) {recreateDB();}
+  
+
 
 module.exports = app;
